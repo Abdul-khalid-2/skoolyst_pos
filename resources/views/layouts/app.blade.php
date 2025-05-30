70697e6c72529b68ae88cb0206cf96179586f289
Khalid Exordium <khalidexordium@gmail.com>
2025-05-30 11:43:27 -0700

    remove select 2 library

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,14 +129,7 @@
    
 
     @stack('js')
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('.select_two_functionality').select2();
-        });
-    </script>
+
 </body>
 
 </html>
