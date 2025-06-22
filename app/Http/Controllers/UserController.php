d53fb481fb2384b49b98dc66271f92d8ea7b84a4
AK Project <abdulkhalidmasood66@gmail.com>
2025-06-22 20:28:17 +0500

    User Update and Show blade is Now Done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,13 +64,13 @@ class UserController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        return view('admin.users.show', compact('user'));
     }
 
     public function edit(User $user)
     {
         $roles = Role::all();
-        return view('users.edit', compact('user', 'roles'));
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     public function update(Request $request, User $user)
