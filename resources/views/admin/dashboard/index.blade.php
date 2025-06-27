bc4dbe53552977a74f32a503842bfa3376a580ca
khalidexordium <khalidexordium@gmail.com>
2025-06-27 15:40:56 -0700

    dashbord and sidebare updates

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -91,15 +91,17 @@
                 <div class="card card-transparent card-block card-stretch card-height border-none">
                     <div class="card-body p-0 mt-lg-2 mt-0">
                         <h3 class="mb-3">Hi {{ auth()->user()->name }}, Good 
-                            @if(now()->hour > 6 && now()->hour <12)
-                                Morning
-                            @elseif(now()->hour < 17)
-                                Afternoon
-                            @elseif(now()->hour < 21)
-                                Evening
-                            @else
-                                Night see you tommarow
-                            @endif
+                            @php
+                                $hour = now()->hour;
+                                $greeting = match(true) {
+                                    $hour >= 5 && $hour < 12 => 'Morning',
+                                    $hour >= 12 && $hour < 17 => 'Afternoon',
+                                    $hour >= 17 && $hour < 21 => 'Evening',
+                                    default => 'Night'
+                                };
+                            @endphp
+
+                            {{ $greeting }}
                         </h3>
                         <p class="mb-0 mr-4">Your dashboard gives you views of key performance or business process.</p>
                     </div>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -210,7 +210,7 @@
                             <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
                             <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
                         </svg>
-                        <span class="ml-4">People</span>
+                        <span class="ml-4">User Management</span>
                         <svg class="svg-icon iq-arrow-right arrow-active" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <polyline points="10 15 15 20 20 15"></polyline>
                             <path d="M4 4h7a4 4 0 0 1 4 4v12"></path>
